aa8139c01e5a2c5f9ef051bf5c441ce7468df702
Gustavo Hammes <sistema.gustavo@gmail.com>
2024-08-06 09:20:01 -0300

    FIX-PERF
    Uma mudança de código que melhora a performance, corrige um bug.

--- mysql/mysql-init/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS db;
-USE db;
-
--- Verifica se o usuário existe antes de tentar criá-lo
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'pass_root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
